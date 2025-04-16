de83b8418ed066ddd7caec84007ee53af5200eb3
Lasha Efremidze <efremidzel@hotmail.com>
2025-04-16 01:06:36 -0700

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/health-score-viz/', // Replace with your GitHub repository name
+  base: '/insight/', // Set for GitHub Pages deployment
 })
